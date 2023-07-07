1d717f7dc9d2b0e20a9b5e509ac7adc48a1a73af
art3mes <93867313+art3mes@users.noreply.github.com>
2023-07-07 23:28:10 +0530

    data is now being sent to mongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const cors =require ("cors");           //to enable access from multiple domains
 const bodyParser =require("body-parser");
+const mongoose=require("mongoose");
 
 const app=express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+
+mongoose.connect('mongodb+srv://admin-arty:admin-arty@cluster0.569qwxi.mongodb.net/KeeperApp');
+const noteSchema={                               
+    title:String,
+    content:String
+};
+
+const Note=mongoose.model('Note',noteSchema);
 
 const corsOptions ={
     origin:'*',
@@ -18,9 +28,20 @@ app.get("/",function(req,res){
 }); 
 
 app.post("/data", function(req,res){
-    const title=req.body.title;
-    const content=req.body.content;
-    console.log(title," ",content);
+    const titleName=req.body.title;
+    const contentData=req.body.content;
+    //console.log(titleName," ",contentData);
+    Note.findOne({title:titleName }).then((foundNote)=>{
+        if(!foundNote){
+            //make a new list
+            const note =new Note({          //creating new list whenever new custom list is called
+               title:titleName,
+               content:contentData
+            });
+            note.save();  //saving the made list to db
+            res.redirect("/"); //redirect to current directory
+        }
+    });                   
 });
 
 app.listen(4000,function(){
