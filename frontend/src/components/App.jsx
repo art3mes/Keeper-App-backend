a8ff5f3d39088e4e4a14313f18297d39b46267d6
art3mes <93867313+art3mes@users.noreply.github.com>
2023-07-09 20:13:46 +0530

    data sent by backend is being recieved and shown by frontend

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,20 +3,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import axios from "axios";
 
 function App() {
   const [notes, setNotes] = useState([]); //empty array
-  //const baseURL="http://localhost:4000/";
+  const baseURL="http://localhost:4000/";
   function addNote(newNote) {
     setNotes((preValue) => {
       return [...preValue, newNote];
     });
   }
-  // React.useEffect(() => {
-  //   axios.get(baseURL).then((response) => {
-  //     setNotes(response.data);
-  //   });
-  // }, []);
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setNotes(response.data);
+    });
+  }, []);
 
   function deleteNote(id) {
     setNotes((preValues) => {
